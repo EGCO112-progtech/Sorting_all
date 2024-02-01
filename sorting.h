29d7a6a4131ed9054a66f290f35f3509a9b025bf
AjMing <62439174+AjMing@users.noreply.github.com>
2024-02-01 21:31:56 +0700

    Create sorting.h

---
+++ sorting.h
@@ -0,0 +1,22 @@
+void display(int [],int );
+void swap(int &,int &);
+void bubblesorting(int a[],int N);
+
+void display(int a[],int N){
+        int i;
+       for(i=0;i<N;i++)
+           cout<<a[i]<<" ";
+       cout<<endl;
+    
+}
+
+void swap(int & x,int &y){
+        int temp=y;
+        y=x;
+        x=temp.
+    
+}
+void bubbleSort(int a[],int N){
+
+
+}
